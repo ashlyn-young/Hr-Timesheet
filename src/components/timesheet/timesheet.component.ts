253a1757cc434f7b508e6ee472459ccca41176be
Howler234 <howler234@gmail.com>
2023-08-29 11:33:29 -0600

    added styling to pages and routers for departments

--- src/components/timesheet/timesheet.component.ts
+++ src/components/timesheet/timesheet.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Department } from 'src/interfaces/department';
+import { DepartmentsService } from 'src/services/departments.service';
+
 
 @Component({
   selector: 'app-timesheet',
@@ -6,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./timesheet.component.scss']
 })
 export class TimesheetComponent {
+  departments: Department[];
+  department: Department;
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private departmentsService: DepartmentsService,
+  ){}
+  ngOnInit(): void {
+    this.departments = this.departmentsService.departments;
+    this.department = this.departments.find(department => department.id === this.route.snapshot.params['id']);
+
+  }
 
 }
