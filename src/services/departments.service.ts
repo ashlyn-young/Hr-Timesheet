dc01f5f9765ae973dcdd1ad770739575283f3810
Howler234 <howler234@gmail.com>
2023-09-19 09:53:28 -0600

    removed hard coded departments after throwing in an http request instead

--- src/services/departments.service.ts
+++ src/services/departments.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Department } from '../interfaces/department';
-import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepartmentsService {
-  departments: Department[] = [
-    {id: '1', name: 'Customer Success'},
-    {id: '2', name: 'Sales'},
-    {id: '3', name: 'Finance'},
-];
+  departments: Department[] = [];
 
   constructor(private http: HttpClient) {
     
+   }
+   getDepartments(): Observable<Department[]> {
+    return this.http.get<Department[]>(`https://hr-timesheet-test.firebaseio.com/departments.json`);
    }
 }
