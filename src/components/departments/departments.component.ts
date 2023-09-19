5c4778a564ecff5baba70ef5d6335bb05a10d26a
Howler234 <howler234@gmail.com>
2023-09-19 10:00:30 -0600

    added a prefix to departments variable, made getdepartments async

--- src/components/departments/departments.component.ts
+++ src/components/departments/departments.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { DepartmentsService } from '../../services/departments.service';
 import { Department } from 'src/interfaces/department';
 import { Router } from '@angular/router';
+import {Observable} from 'rxjs';
+
+
 
 @Component({
   selector: 'app-departments',
@@ -9,16 +12,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./departments.component.scss']
 })
 export class DepartmentsComponent {
-  departments: Department[];
+  departments: Department[] | undefined;
+  $departments: Observable<Department[]> | undefined;
   
   constructor(
     private departmentsService: DepartmentsService,
     private router: Router
   ) {}
   ngOnInit(): void {
-    this.departmentsService.getDepartments().subscribe(departments => {
-      this.departments = departments;
-  });
+    // this.departmentsService.getDepartments().subscribe(departments => {
+    //   this.departments = departments;
+    // });
+  this.$departments = this.departmentsService.getDepartments();
   }
   goToDepartment(departmentId: string):void {
     this.router.navigate(['./timesheet', {id: departmentId}]);
