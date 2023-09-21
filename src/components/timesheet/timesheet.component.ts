05346cb3c997119a51d0a49f5041452acea88de3
Howler234 <howler234@gmail.com>
2023-09-21 08:15:21 -0600

    final step complete can delete or edit an employee and their hours

--- src/components/timesheet/timesheet.component.ts
+++ src/components/timesheet/timesheet.component.ts
@@ -77,15 +77,23 @@ export class TimesheetComponent {
     getTotalHours(employee: Employee): number {
       return employee.monday+ employee.tuesday + employee.wednesday + employee.thursday + employee.friday + employee.saturday + employee.sunday;
     }
-    deleteEmployee(index: number): void {
+    deleteEmployee(employee: Employee, index: number): void {
+      if (employee.id) {
+          this.employeeService.deleteEmployeeHours(employee);
+      }
+  
       this.employees.splice(index, 1);
-    }
+  }
     submit(): void {
       this.employees.forEach(employee => {
-        this.employeeService.saveEmployeeHours(employee);
+          if (employee.id) {
+              this.employeeService.updateEmployeeHours(employee);
+          } else {
+              this.employeeService.saveEmployeeHours(employee);
+          }
       });
   
       this.router.navigate(['./departments']);
-    }
+  }
   }
 
--- src/services/employee.service.ts
+++ src/services/employee.service.ts
@@ -35,5 +35,11 @@ export class EmployeeService {
             });
         })
     );
+  }
+  updateEmployeeHours(employee: Employee): any {
+    this.db.collection('employee-hours').doc(employee.id).set(employee);
+  }
+  deleteEmployeeHours(employee: Employee): any {
+    this.db.collection('employee-hours').doc(employee.id).delete();
 }
 }
